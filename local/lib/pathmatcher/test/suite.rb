a772354bd629ff8212c9860742ff75641b4bed0f
Shawn Tice <shawn.cameron.bird@gmail.com>
2017-02-20 13:17:10 +0100

    Remove possibly-broken pathmatcher dir

--- local/lib/pathmatcher/test/suite.rb
+++
@@ -1,6 +0,0 @@
-require 'minitest/autorun'
-
-require_relative '../lib/pathmatcher'
-require_relative 'tc_match_algorithm'
-require_relative 'tc_match_algorithm_c'
-require_relative 'tc_cli'
--- local/lib/pathmatcher/test/tc_match_algorithm_c.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'tc_match_algorithm'
-
-class TestPathMatchAlgorithmC < TestPathMatchAlgorithm
-  def setup
-    have_c_ext = defined? PathMatcher::PathMatchC
-    skip 'PathMatchC extension not built?' unless have_c_ext
-    @path_match = PathMatcher::PathMatchC
-  end
-end
